2b0dfefee782112284c1db86e59d4efd93930194
betahikaru <beta.hikaru2000+github@gmail.com>
2015-04-11 17:44:32 +0900

    Fetch AWS IAM Users from AWS API Server

--- server/lib/aws_portal/application.rb
+++ server/lib/aws_portal/application.rb
@@ -6,8 +6,16 @@ require 'sinatra/partial'
 
 require 'json'
 
+require 'aws-sdk'
+require 'dotenv'
+
 module AwsPortal
   class Application < Sinatra::Base
+    # for Reload
+    configure :development do
+        register Sinatra::Reloader
+    end
+
     # for "sinatra/content-for"
     register Sinatra::Contrib
 
@@ -18,6 +26,9 @@ module AwsPortal
     # setting for directory path
     set :root, File.join(File.dirname(__FILE__), "..", "..")
     set :views, Proc.new { File.join(root, "views") }
+
+    # dotenv
+    Dotenv.load
 
     get '/' do
       content_type :json
@@ -48,7 +59,27 @@ module AwsPortal
 
     get '/aws/iam/users' do
       content_type :json
-      erb :"aws/iam/users/list.json"
+      begin
+        client = Aws::IAM::Client.new
+        resource = Aws::IAM::Resource.new(client: client)
+        users_obj = resource.users
+        users = []
+        users_obj.each do |user_obj|
+          users.push({
+            UserName: user_obj.name,
+            Path: user_obj.path,
+            CreateDate: user_obj.create_date,
+            UserId: user_obj.user_id,
+            Arn: user_obj.arn
+          })
+        end
+        {
+          Users: users
+        }.to_json
+      rescue => error
+        p error
+        return {}.to_json
+      end
     end
 
   end
