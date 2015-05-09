f4043b3dbd29b8e5b85699d0581685bbfe5926db
betahikaru <beta.hikaru2000+github@gmail.com>
2015-05-09 14:16:24 +0900

    Support to jump Settings.app, by NativeModule

---
+++ iOS/OpenSettingAppModule.h
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2015, betahikaru.
+ * All rights reserved.
+ * MIT
+ */
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "RCTBridgeModule.h"
+#import "RCTLog.h"
+
+@interface OpenSettingAppModule : NSObject<RCTBridgeModule>
+
+@end
